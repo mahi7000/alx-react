27fe6f141adb4a137f5a453178c8dc1082294d58
mahi7000 <mahlet7000@gmail.com>
2024-10-31 17:25:58 +0300

    task 2

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <div id='root-notifications'>
+      <Notifications />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
