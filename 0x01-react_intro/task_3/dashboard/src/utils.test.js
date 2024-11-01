0bde98255d9d036ec71511424eda9b36e1939223
mahi7000 <mahlet7000@gmail.com>
2024-11-01 11:13:52 +0300

    Test utils

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,5 +1,16 @@
 import {getFullYear, getFooterCopy, getLatestNotification} from './utils'
 
-describe('getFullYear', () => {
-    
-})
+test('returns current year', () => {
+    expect(getFullYear()).toBe(2024);
+});
+
+test('returns footer', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School');
+    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+});
+
+test('returns the latest notification', () => {
+    expect(getLatestNotification()).toBe(
+        '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+});
