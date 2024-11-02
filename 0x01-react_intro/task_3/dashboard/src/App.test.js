01f245f85852cbfe24eaba310e7ec59256b755de
mahi7000 <mahlet7000@gmail.com>
2024-11-02 12:16:41 +0300

    set up test

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -6,3 +6,22 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('App renders a div with the class App-header', () => {
+  const { getByTestId } = render(<App />);
+  const appHeader = getByTestId('App-header');
+  expect(appHeader).toBeInTheDocument();
+});
+
+test('App renders a div with the class App-body', () => {
+  const { getByTestId } = render(<App />);
+  const appBody = getByTestId('App-body');
+  expect(appBody).toBeInTheDocument();
+});
+
+test('App renders a div with the class App-footer', () => {
+  const { getByTestId } = render(<App />);
+  const appFooter = getByTestId('App-footer');
+  expect(appFooter).toBeInTheDocument();
+});
