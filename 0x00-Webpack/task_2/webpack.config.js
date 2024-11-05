4fefe6eceebc349096a07f5bf8a0747b9c89a010
mahi7000 <mahlet7000@gmail.com>
2024-11-05 22:23:00 +0300

    Add css and images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
