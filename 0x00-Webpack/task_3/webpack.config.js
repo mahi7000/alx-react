4900424860b000c3bbef559efd73a01f20b07bd2
mahi7000 <mahlet7000@gmail.com>
2024-11-06 07:19:13 +0300

    devtool

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    devtool: 'source-map',
     entry: {
         header: {
           import: './modules/header/header.js',
