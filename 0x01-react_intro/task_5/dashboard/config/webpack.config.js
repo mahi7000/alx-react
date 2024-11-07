9d4fe65083b3657ded50b564ffdc0f6be947449d
mahi7000 <mahlet7000@gmail.com>
2024-11-07 15:03:01 +0300

    Add task_5

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader, css-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|)/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        }
+                    },
+                ]
+            },
+        ]
+    },
+    devServer: {
+        static: '../dist',
+        compress: true,
+        open: true,
+        hot: true,
+        port: 8564,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            name: 'index.html',
+            inject: false,
+            template: '../dist/index.html',
+        }),
+    ],
+};
