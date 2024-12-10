3d628f005b5e7f61668eba86c10bd1db6da197cb
mahi7000 <mahlet7000@gmail.com>
2024-12-10 10:17:56 +0300

    Combine all reducers

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Map } from 'immutable';
+import App from './App/App';
+import { initialState, uiReducer } from './reducers/uiReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  uiReducer,
+  Map(initialState),
+  composeEnhancers(applyMiddleware(thunk))
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
